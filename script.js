657d4d8ca6d25363d09c919d27853cad8cf6f0db
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2024-09-29 18:19:19 +0530

    Add play game function.
    added play game function which contains variables and round functions.
    its working.
    !! - "But not called playRound five times. Instead called playGame func
    five times"
    
    Note: forget to add arguments in playRound function.So function not
    working as expected

--- script.js
+++ script.js
@@ -23,38 +23,60 @@ function getHumanChoice (){
     return humanChoice.toLowerCase();
 }
 
-function playRound (humanChoice, compChoice) {
-    if (humanChoice == "rock" && compChoice == "scissors"){
-        return console.log("You win, Rock breaks scissors!");
-    }else if (humanChoice == "paper" && compChoice == "rock"){
-        return console.log("You win, Paper covers rock!");
-    }else if(humanChoice == "scissors" && compChoice == "paper"){
-        return console.log("You win, scissors cut paper!");
-    }else if (humanChoice == "rock" && compChoice == "paper"){
-        return console.log("You lose, paper covers rock!");
-    }else if (humanChoice == "paper" && compChoice == "scissors"){
-        return console.log("You lose, scissors cut paper!");
-    }else if (humanChoice == "scissors" && compChoice == "rock"){
-        return console.log("You lose, rock breaks scissors!");
-    }else if (humanChoice == compChoice){
-        return console.log("Its a Draw!");
-    }else{
-        return console.log("No valid choice made. Try again!");
+
+function playGame () {
+
+    const humanChoice = getHumanChoice(); 
+    const compChoice = getComputerChoice();
+    console.log(compChoice);
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound (humanChoice, compChoice) {
+        if (humanChoice == "rock" && compChoice == "scissors"){
+            humanScore ++ ;
+            return console.log("You win, Rock breaks scissors!");
+        }else if (humanChoice == "paper" && compChoice == "rock"){
+            humanScore ++ ;
+            return console.log("You win, Paper covers rock!");
+        }else if(humanChoice == "scissors" && compChoice == "paper"){
+            humanScore ++ ;
+            return console.log("You win, scissors cut paper!");
+        }else if (humanChoice == "rock" && compChoice == "paper"){
+            computerScore ++ ;
+            return console.log("You lose, paper covers rock!");
+        }else if (humanChoice == "paper" && compChoice == "scissors"){
+            computerScore ++ ;
+            return console.log("You lose, scissors cut paper!");
+        }else if (humanChoice == "scissors" && compChoice == "rock"){
+            computerScore++ ;
+            return console.log("You lose, rock breaks scissors!");
+        }else if (humanChoice == compChoice){
+            return console.log("Its a Draw!");
+        }else{
+            return console.log("No valid choice made. Try again!");
+        }
     }
+
+    
+
+    return playRound(humanChoice, compChoice);
 }
 
-const humanChoice = getHumanChoice(); 
-const compChoice = getComputerChoice();
-console.log(compChoice);
-
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
 // console.log (typeof(humanChoice));
 // console.log(typeof(compChoice));
 
 // getHumanChoice();
 // getComputerChoice();
-playRound(humanChoice, compChoice);
+// playRound(humanChoice, compChoice);
 
-let humanScore = 0;
-let computerScore = 0;
+
+
 
 
