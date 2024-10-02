2c148fd359f2ee2481a299bb7976a052f1f6f5b9
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2024-10-02 11:40:46 +0530

    Add buttons to rps game to get human choice
    Added buttons for each human choice and removed human choice function
    So far buttons are working. But only one comp choice is made and all
    button choices compares to it. Need to  cha roundnge to take each

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function getComputerChoice (){
 
     let compChoice = Math.random() ;
-    // let rock = "rock";
-    // let paper = "paper";
-    // let scissors = "scissors";
-    // console.log(compChoice);
-
+    
     if (compChoice >= 0 && compChoice < 0.3){
         return "rock" ;
     } else if (compChoice >= 0.3 && compChoice < 0.6){
@@ -18,20 +14,51 @@ function getComputerChoice (){
 
 
 
-function getHumanChoice (){
-    let humanChoice = prompt("Choose rock or paper or scissors", "rock");
-    return humanChoice.toLowerCase();
-}
+   
 
 
-function playGame () {
 
-    const humanChoice = getHumanChoice(); 
+
+
+
     const compChoice = getComputerChoice();
     console.log(compChoice);
 
+
     let humanScore = 0;
     let computerScore = 0;
+
+    let buttons = document.querySelector(".buttons");
+
+    buttons.addEventListener("click", (event) => {
+        let target = event.target;
+
+        switch (target.id){
+            case "rock":
+                playRound("rock", compChoice);
+                break;
+            
+            case "paper":
+                playRound("paper", compChoice);
+                break;
+
+            case "scissors":
+                playRound("scissors", compChoice);
+                break;
+
+            default:
+                console.log("Click button");
+        }
+    });
+
+    // const rockButton = document.querySelector(".rock");
+    // rockButton.addEventListener("onclick", playRound("rock", compChoice));
+
+    // const paperButton = document.querySelector(".paper");
+    // paperButton.addEventListener("onclick", playRound("paper", compChoice));
+
+    // const scissorsButton = document.querySelector(".scissors");
+    // scissorsButton.addEventListener("onclick", playRound("scissors", compChoice));
 
     function playRound (humanChoice, compChoice) {
         if (humanChoice == "rock" && compChoice == "scissors"){
@@ -61,22 +88,5 @@ function playGame () {
 
     
 
-    return playRound(humanChoice, compChoice);
-}
-
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
-// console.log (typeof(humanChoice));
-// console.log(typeof(compChoice));
-
-// getHumanChoice();
-// getComputerChoice();
-// playRound(humanChoice, compChoice);
-
-
-
-
+    
 
