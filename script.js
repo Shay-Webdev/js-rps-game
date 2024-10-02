381b0e9e9e8effca8dffa735163964bc8197688c
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2024-10-02 13:32:32 +0530

    Finish adding buttons functions to rps game
    Finished adding buttons and updating score to we page.

--- script.js
+++ script.js
@@ -13,25 +13,27 @@ function getComputerChoice (){
 }
 
 let buttons = document.querySelector(".buttons");
+let results = document.querySelector(".results");
+let score = document.querySelector(".score");
 
 buttons.addEventListener("click", (event) => {
     let target = event.target;
     switch (target.id){
         case "rock":
             let compChoiceA = getComputerChoice();
-            console.log(compChoiceA);
             playRound("rock", compChoiceA);
+            finalResults();
             break;
 
         case "paper":
             let compChoiceB = getComputerChoice();
-            console.log(compChoiceB);
             playRound("paper", compChoiceB);
+            finalResults();
             break;
         case "scissors":
             let compChoiceC = getComputerChoice();
-            console.log(compChoiceC);
             playRound("scissors", compChoiceC);
+            finalResults();
             break;
         default:
             console.log("Click button");
@@ -46,27 +48,38 @@ let computerScore = 0;
 function playRound (humanChoice, compChoice) {
     if (humanChoice == "rock" && compChoice == "scissors"){
         humanScore ++ ;
-        return console.log("You win, Rock breaks scissors!");
+        return results.textContent = `Computer choose scissors.\n You win, Rock breaks scissors!.\n Human score: ${humanScore}. computer score: ${computerScore}`;
     }else if (humanChoice == "paper" && compChoice == "rock"){
         humanScore ++ ;
-        return console.log("You win, Paper covers rock!");
+        return results.textContent = `Computer choose rock.\n You win, Paper covers rock!\n Human score: ${humanScore}. computer score: ${computerScore}`;
     }else if(humanChoice == "scissors" && compChoice == "paper"){
         humanScore ++ ;
-        return console.log("You win, scissors cut paper!");
+        return results.textContent = `Computer choose scissors.\n You win, scissors cut paper!\n Human score: ${humanScore}. computer score: ${computerScore}`;
     }else if (humanChoice == "rock" && compChoice == "paper"){
         computerScore ++ ;
-        return console.log("You lose, paper covers rock!");
+        return results.textContent = `Computer choose paper.\n You lose, paper covers rock!\n Human score: ${humanScore}. computer score: ${computerScore}`;
     }else if (humanChoice == "paper" && compChoice == "scissors"){
         computerScore ++ ;
-        return console.log("You lose, scissors cut paper!");
+        return results.textContent = `Computer choose scissors.\n You lose, scissors cut paper!\n Human score: ${humanScore}. computer score: ${computerScore}`;
     }else if (humanChoice == "scissors" && compChoice == "rock"){
         computerScore++ ;
-        return console.log("You lose, rock breaks scissors!");
+        return results.textContent = `Computer choose rock.\n You lose, rock breaks scissors!\n Human score: ${humanScore}. computer score: ${computerScore}`;
     }else if (humanChoice == compChoice){
-        return console.log("Its a Draw!");
+        return results.textContent = `Computer choose ${compChoice}.\nIts a Draw!\n Human score: ${humanScore}. computer score: ${computerScore}`;
     }else{
         return console.log("No valid choice made. Try again!");
     }
+}
+
+function finalResults(){
+
+
+    if (humanScore == 5 ){
+       return score.textContent = " Winner is You!";
+    }else if (computerScore == 5){
+        return score.textContent = "Winner is computer!";
+    }
+    
 }
 
 
