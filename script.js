85e0489014d3ef305210d69133c1a6fa205c2085
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2024-10-02 12:35:41 +0530

    Add button functionality to rps game
    Added the functionality button and made computer choice work
    by adding function to a variable and to each switch of buttons
    
    have to add result display in webpage.

--- script.js
+++ script.js
@@ -12,79 +12,64 @@ function getComputerChoice (){
     
 }
 
+let buttons = document.querySelector(".buttons");
 
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+    switch (target.id){
+        case "rock":
+            let compChoiceA = getComputerChoice();
+            console.log(compChoiceA);
+            playRound("rock", compChoiceA);
+            break;
 
-   
-
-
-
-
-
-
-    const compChoice = getComputerChoice();
-    console.log(compChoice);
-
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    let buttons = document.querySelector(".buttons");
-
-    buttons.addEventListener("click", (event) => {
-        let target = event.target;
-
-        switch (target.id){
-            case "rock":
-                playRound("rock", compChoice);
-                break;
-            
-            case "paper":
-                playRound("paper", compChoice);
-                break;
-
-            case "scissors":
-                playRound("scissors", compChoice);
-                break;
-
-            default:
-                console.log("Click button");
-        }
-    });
-
-    // const rockButton = document.querySelector(".rock");
-    // rockButton.addEventListener("onclick", playRound("rock", compChoice));
-
-    // const paperButton = document.querySelector(".paper");
-    // paperButton.addEventListener("onclick", playRound("paper", compChoice));
-
-    // const scissorsButton = document.querySelector(".scissors");
-    // scissorsButton.addEventListener("onclick", playRound("scissors", compChoice));
-
-    function playRound (humanChoice, compChoice) {
-        if (humanChoice == "rock" && compChoice == "scissors"){
-            humanScore ++ ;
-            return console.log("You win, Rock breaks scissors!");
-        }else if (humanChoice == "paper" && compChoice == "rock"){
-            humanScore ++ ;
-            return console.log("You win, Paper covers rock!");
-        }else if(humanChoice == "scissors" && compChoice == "paper"){
-            humanScore ++ ;
-            return console.log("You win, scissors cut paper!");
-        }else if (humanChoice == "rock" && compChoice == "paper"){
-            computerScore ++ ;
-            return console.log("You lose, paper covers rock!");
-        }else if (humanChoice == "paper" && compChoice == "scissors"){
-            computerScore ++ ;
-            return console.log("You lose, scissors cut paper!");
-        }else if (humanChoice == "scissors" && compChoice == "rock"){
-            computerScore++ ;
-            return console.log("You lose, rock breaks scissors!");
-        }else if (humanChoice == compChoice){
-            return console.log("Its a Draw!");
-        }else{
-            return console.log("No valid choice made. Try again!");
-        }
+        case "paper":
+            let compChoiceB = getComputerChoice();
+            console.log(compChoiceB);
+            playRound("paper", compChoiceB);
+            break;
+        case "scissors":
+            let compChoiceC = getComputerChoice();
+            console.log(compChoiceC);
+            playRound("scissors", compChoiceC);
+            break;
+        default:
+            console.log("Click button");
     }
+});
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, compChoice) {
+    if (humanChoice == "rock" && compChoice == "scissors"){
+        humanScore ++ ;
+        return console.log("You win, Rock breaks scissors!");
+    }else if (humanChoice == "paper" && compChoice == "rock"){
+        humanScore ++ ;
+        return console.log("You win, Paper covers rock!");
+    }else if(humanChoice == "scissors" && compChoice == "paper"){
+        humanScore ++ ;
+        return console.log("You win, scissors cut paper!");
+    }else if (humanChoice == "rock" && compChoice == "paper"){
+        computerScore ++ ;
+        return console.log("You lose, paper covers rock!");
+    }else if (humanChoice == "paper" && compChoice == "scissors"){
+        computerScore ++ ;
+        return console.log("You lose, scissors cut paper!");
+    }else if (humanChoice == "scissors" && compChoice == "rock"){
+        computerScore++ ;
+        return console.log("You lose, rock breaks scissors!");
+    }else if (humanChoice == compChoice){
+        return console.log("Its a Draw!");
+    }else{
+        return console.log("No valid choice made. Try again!");
+    }
+}
+
+
 
     
 
